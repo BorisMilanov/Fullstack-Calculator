b58293cc0c8d715f7416b9a8ed9e8bcecf9caead
BorisMilanov <68430756+BorisMilanov@users.noreply.github.com>
2024-10-14 16:47:21 +0300

    Update BackendApplication.java

--- backend/src/main/java/com/backend/BackendApplication.java
+++ backend/src/main/java/com/backend/BackendApplication.java
@@ -2,7 +2,7 @@ package com.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import com.backend.controller.CalculatorController;
 @SpringBootApplication
 public class BackendApplication {
 
